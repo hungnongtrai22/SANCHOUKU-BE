8487c3d6ddc0d5182f3b1594a35cb345a8b7f72d
VU QUOC HUNG <vuquochung220402@gmail.com>
2026-04-03 09:35:24 +0700

    Update create.ts

--- src/pages/api/center/create.ts
+++ src/pages/api/center/create.ts
@@ -1,29 +1,34 @@
 import { NextApiRequest, NextApiResponse } from 'next';
-// utils
 import cors from 'src/utils/cors';
 import Center from 'src/models/Center';
-
-// _mock
 import db from '../../../utils/db';
-// ----------------------------------------------------------------------
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   try {
     await cors(req, res);
-
     await db.connectDB();
 
     const { regulation } = req.body;
-    const newCenter = await new Center({
-      regulation,
-   
-    }).save();
+
+    // tìm center đầu tiên
+    let center = await Center.findOne();
+
+    if (center) {
+      // nếu đã tồn tại → update
+      center.regulation = regulation;
+      await center.save();
+    } else {
+      // nếu chưa có → tạo mới
+      center = await new Center({
+        regulation,
+      }).save();
+    }
 
     return res.status(200).json({
-      center: newCenter,
+      center,
     });
   } catch (error) {
-    console.error('[Auth API]: ', error);
+    console.error('[Center API]: ', error);
     return res.status(400).json({
       message: error,
     });
